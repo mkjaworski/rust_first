e34351721656d080b5751ae0db6e5dad631e3c27
MKJ <mkjaworski@icloud.com>
2022-09-11 22:15:35 +0200

    third.rs added

---
+++ src/third.rs
@@ -0,0 +1,9 @@
+pub fn prt1() {
+    //Print to the console
+    println!("Hello from the second.rs file"); 
+}
+
+pub fn prt2() {
+    //Print to the console
+    println!("This is also from second.rs, but with number: {}", 1); 
+}
